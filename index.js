f471802dae5517a7d97c1778a8c2011e51635b0a
guitch95 <guitch95@gmail.com>
2025-10-31 15:46:19 +0100

    New test

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ cloudinary.config({
 });
 
 app.get('/', (req, res) => {
-  res.status(200).json({message: 'Bienvenue sur mon serveur Vinted les amis'});
+  res.status(200).json({message: 'Bienvenue sur mon serveur Vinted'});
 });
 
 app.use(userRoutes);
